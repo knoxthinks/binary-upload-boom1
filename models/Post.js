a8f2a3bb9057823e2aee5bc86d01dc1fdedd6117
knoxLaptop <itsknox@gmail.com>
2023-11-17 13:57:39 -0500

    notes on models/post

--- models/Post.js
+++ models/Post.js
@@ -23,12 +23,18 @@ const PostSchema = new mongoose.Schema({
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
+    //reference to other schemas.
+    //in this case it is User.js in models
     ref: "User",
   },
   createdAt: {
     type: Date,
+    //if you did not set a date, it will set one for you
     default: Date.now,
   },
 });
-
+//exporting - a new model,
+//naming it "Post",
+//uses the "PostSchema",
+//"specify a collection"
 module.exports = mongoose.model("Post", PostSchema);
