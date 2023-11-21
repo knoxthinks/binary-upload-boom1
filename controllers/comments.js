196d54f65c6853b742b245ca7242fd06424ac82e
knoxLaptop <itsknox@gmail.com>
2023-11-21 14:48:04 -0500

    C:/Program Files/Git/controller/comment.js/ added the data to input new data to schema

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,11 +3,15 @@ const Comment = require("../models/Comment");
 module.exports = {
   createComment: async (req, res) => {
     try {
+      //taken from controller/post.js
+      // const commentUser = await User.findById(req.user.id);
       await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
         //TODO - add new properties to each new document
+        createdBy: req.user.userName,
+        createdByID: req.user.id,
       });
       console.log("Comment has been added!");
       res.redirect("/post/" + req.params.id);
